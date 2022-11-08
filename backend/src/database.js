37c1c57540f798a8f0876884337fb9e2e10018a8
jagrunewald <je_grunewald@hotmail.com>
2022-11-08 10:21:02 -0300

    Creating node project, installed express and nodemon, created endpoint to list and search students

---
+++ backend/src/database.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    nome: 'Jessica Aline',
+    email: 'jessica@gmail.com',
+    cpf: '000.000.000-10',
+    ra: '1234',
+  },
+  {
+    nome: 'Maria Silva',
+    email: 'maria@gmail.com',
+    cpf: '000.000.000-20',
+    ra: '1235',
+  },
+  {
+    nome: 'João Carlos',
+    email: 'joao@gmail.com',
+    cpf: '000.000.000-30',
+    ra: '1236',
+  },
+];
