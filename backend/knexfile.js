4c54a7fca5e9406b64d4e08dda839bbfee074117
jagrunewald <je_grunewald@hotmail.com>
2022-11-24 10:02:32 -0300

    Installed KNEX and MySQL, created a students migration

---
+++ backend/knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  test: {
+    client: "mysql",
+    connection: {
+      host: "remotemysql.com",
+      user: "3TmgQRBim1",
+      password: "KQFycn681j",
+      database: "3TmgQRBim1",
+    },
+    migrations: {
+      directory: "migrations",
+    },
+  },
+};
