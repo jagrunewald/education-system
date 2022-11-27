d27464fd942832a13f73c9984f71ae63ee637696
jagrunewald <je_grunewald@hotmail.com>
2022-11-27 16:42:05 -0300

    Migration of delete and insert to the database | Implemented field validation | Implemented LIKE on student list endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,21 +16,28 @@ app.get("/", (req, res) => {
 })
 
 app.get("/students/list/:searchQuery?", (req, res) => {
+  let query = app.database("students");
+    
   let result = database;
   let search = req.params.searchQuery;
-  
+
   if(search) {
-    search = search.toLowerCase();
-    result = result.filter((student) => {
-      return (
-        student.ra == search ||
-        student.nome.toLowerCase().indexOf(search) != -1 ||
-        student.cpf == search
-      );
-    })
+    query.where("ra", search)
+      .orWhere("nome", "like", `%${search}%`)
+      .orWhere("cpf", search);
+
+    // search = search.toLowerCase();
+    // result = result.filter((student) => {
+    //   return (
+    //     student.ra == search ||
+    //     student.nome.toLowerCase().indexOf(search) != -1 ||
+    //     student.cpf == search
+    //   );
+    // })
   }
 
-  return app.database("students").select()
+  return query
+    .select()
     .then((data) => {
       res.send(data);
     });
@@ -46,30 +53,102 @@ app.get("/students/find/:ra", (req, res) => {
 })
 
 app.delete("/students/delete/:ra", (req, res) => {
-  database = database.filter((student) => {
-    return student.ra != req.params.ra;
-  });
-  res.send({
-    result: true,
-    message: `O estudante ${req.params.ra} foi excluído com sucesso`,
-  });
+  return app.database("students")
+    .where({ ra: req.params.ra })
+    .del()
+    .then((response) => {
+      if (response) {
+        res.send({
+          result: true,
+          message: `O estudante ${req.params.ra} foi excluído com sucesso`,
+        });
+      } else {
+        res.send({
+          result: false,
+          message: "Nã foi possível excluir o estudante",
+        });
+      }
+    });
 });
 
-app.post("/students/save", (req,res) => {
-  database.push({
-    nome: req.body.name,
-    ra: req.body.ra,
-    email: req.body.email,
-    cpf: req.body.cpf,
-  });
+app.post("/students/save", async (req,res) => {
+  if(req.body.name == '') {
+    return res.status(400).send({
+      result: false,
+      message: "O nome é um campo obrigatório",
+    });
+  }
 
-  res.send({
-    result: true,
-    message: "Estudante cadastrado com sucesso"
-  });
+  if(req.body.email == '') {
+    return res.status(400).send({
+      result: false,
+      message: "O email é um campo obrigatório",
+    });
+  }
+
+  if(req.body.ra == '' && parseInt(req.body.ra) != req.body.ra) {
+    return res.status(400).send({
+      result: false,
+      message: "O RA é um campo obrigatório e precisa ser um número inteiro",
+    });
+  }
+
+  if(req.body.cpf == '' && parseInt(req.body.cpf) != req.body.cpf) {
+    return res.status(400).send({
+      result: false,
+      message: "O cpf é um campo obrigatório e precisa ser um numero inteiro",
+    });
+  }
+
+  const userExists = await app.database("students")
+    .select()
+    .where({ ra: req.body.ra })
+    .first();
+
+  if(userExists) {
+    return res.status(400).send({
+      result: false,
+      message: "Desculpe, mas já existe um estudante cadastrado com esse RA."
+    })
+  }
+
+  return app.database("students")
+    .insert({
+      nome: req.body.name,
+      ra: req.body.ra,
+      email: req.body.email,
+      cpf: req.body.cpf,
+    })
+    .then((response) => {
+      if(response) {
+        res.send({
+          result: true,
+          message: "Estudante cadastrado com sucesso",
+        })
+      } else {
+        res.send({
+          result: false,
+          message: "Não foi possível cadastrar o estudante"
+        });
+      }
+    });
 });
 
 app.put("/students/edit/:ra", async(req, res) => {
+  if(req.body.name == '') {
+    return res.status(400).send({
+      result: false,
+      message: "O nome é um campo obrigatório",
+    });
+  }
+
+  if(req.body.email == '') {
+    return res.status(400).send({
+      result: false,
+      message: "O email é um campo obrigatório",
+    });
+  }
+  
   const userFound = await app
     .database("students")
     .select()
