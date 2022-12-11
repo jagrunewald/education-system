bc77fde811fb0f389df3bd0c8ecc0b4ec4dd9203
jagrunewald <je_grunewald@hotmail.com>
2022-12-11 09:33:09 -0300

    Created Navbar component and its style file

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <section class="container">
+        <header class="main-header">
+          Consulta de Alunos
+        </header>
+        <div class="loader"></div>
+        <div class="content-page display-none">
+          <div class="top-actions">
+            <form  id="form-search-student" class="form-search">
+              <input type="text" name="searchInput" id="searchInput" />
+              <button>Pesquisar</button>
+            </form>
+            <a href="studentManager.html" class="btn">Cadastrar Aluno</a>
+          </div>
+          <table id="studentList" class="table-list">
+            <thead>
+              <tr>
+                <th>Registro acadêmico</th>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>          
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ frontend/react-app/src/components/Navbar/index.jsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+    <header>
+      Módulo academico
+    </header>
+    <ul className="nav-links">
+      <a href="#" className="nav-item">
+        <li>Alunos</li>
+      </a>
+    </ul>
+  </nav>
+  );
+};
+
+export default Navbar;
